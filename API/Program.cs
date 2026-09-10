7a4e9bbc5972deeac4012849b1da72448a3c6533
ADESSO\mkowalski <matthias.kowalski@adesso.de>
2026-09-10 18:15:08 +0200

    Added ErrorHandling and CORS

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,11 +16,14 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     );
 });
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseMiddleware<ExceptionMiddleWare>();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.MapControllers();
 
 try
